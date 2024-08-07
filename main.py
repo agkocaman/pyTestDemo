e8209bcb54287afb40213ba4de63ab63b400aada
Gokhan Kocaman <ggokhankocaman@gmail.com>
2024-08-07 22:57:31 +0300

    title and current url control

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ service = Service("./drivers/chromedriver")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://www.python.org")
+
+link = driver.current_url
+title = driver.title
+if "python.org" in link:
+    print("Bu sayfa Python.org sitesine aittir.1")
+if "Python.org" in title:
+    print("Bu sayfa Python.org sitesine aittir.2")
+driver.get("https://www.google.com")
+link = driver.current_url
+title = driver.title
+print(title+"deneme")
+if "google" in link:
+    print("Bu sayfa google sitesine aittir.3")
+if "Google" in title:
+    print("Bu sayfa google sitesine aittir.4")
+    
