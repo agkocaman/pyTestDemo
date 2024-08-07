f3f4b7d645b38aee70958ef797706edff4cac8a5
Gokhan Kocaman <ggokhankocaman@gmail.com>
2024-08-07 23:21:38 +0300

    search Box send keys

---
+++ aramaKutusu.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+service = Service("./drivers/chromedriver")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://duckduckgo.com/")
+
+aramaKutusu = driver.find_element(By.ID, "searchbox_input")
+aramaKutusu.send_keys("selenium")
+aramaKutusu.send_keys(Keys.ENTER)
+
+title = driver.title
+if "selenium at DuckDuckGo" in title:
+    print("Arama sonucunda selenium kelimesi gosterilmistir.")
