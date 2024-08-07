1a3d48aace3c6abbf131eb91246b6ce62fa429a3
Gokhan Kocaman <ggokhankocaman@gmail.com>
2024-08-07 22:43:18 +0300

    first code

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-test
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service("./drivers/chromedriver")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://www.python.org")
